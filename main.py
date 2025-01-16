677ff6a358d9c3caa77ac300e51732ebe6c7c6fb
Mason Christopherson <masonxiv.14@icloud.com>
2025-01-16 16:38:33 -0700

    add changes to lines 13-15

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from game.camera.camera import Camera
 
 pg.init()
 
+# Mason's Changes
+OTHER_TEMP = "Mason made some changes as well"
+
 # definig some strings so that we can see which class to display
 STATE_MENU = 'menu'
 STATE_GAME = "game"
